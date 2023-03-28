b7a1707fc800db9d292a618b3e9353492a83abe7
masonwgomes <122919650+masonwgomes@users.noreply.github.com>
2023-03-28 17:33:27 -0400

    first update

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const rightColumn = document.querySelector('.right-column');
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
-function clickyCode() {
-    if (rightColumn.style.backgroundColor == 'red'){
-        rightColumn.style.backgroundColor = 'green';
-    } else {
-        rightColumn.style.backgroundColor = 'red';
-    }
+let currentSlide = 0;
+let maxSlide = slides.length - 1;
+
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+function moveSlides() {
+  if (currentSlide === maxSlide) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  });
 }
 
-rightColumn.addEventListener('click', clickyCode);
+nextBtn.addEventListener("click", moveSlides);
+
+setInterval(() => {
+  moveSlides();
+}, 15000);
